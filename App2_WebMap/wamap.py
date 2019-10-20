8e2a14ff070428b2d886b6c9987dc4a0446a3ca4
harbak62 <harbak62@hotmail.com>
2019-10-20 12:43:06 -0700

    Map first commited

--- App2_WebMap/wamap.py
+++ App2_WebMap/wamap.py
@@ -1,9 +1,18 @@
 import folium
+import pandas
 
-map = folium.Map(location=[48.0020547,-122.220774], zoom_start=12, tiles="Stamen Terrain")
+map = folium.Map(location=[42.0020547,-120.220774], zoom_start=5, tiles="Stamen Terrain")
+vdata = pandas.read_csv("Volcanoes.txt")
+lat = list(vdata["LAT"])
+lon = list(vdata["LON"])
+elv = list(vdata["ELEV"])
+name = list(vdata["NAME"])
 
 fg = folium.FeatureGroup(name="Wa Map")
-fg.add_child(folium.Marker(location=[48.0006705,-122.2173022], popup="Port of Everett", icon=folium.Icon(color="green")))
-map.add_child(fg)
 
+for lt, ln, el, na in zip(lat, lon, elv, name):
+    fg.add_child(folium.Marker(location=[lt,ln], popup="<span style='display: inline; width:200px; height:25px'>"+na + "<br/>Elevation " +str(el)+"</span>", icon=folium.Icon(color="green")))
+
+
+map.add_child(fg)
 map.save("WaMap.html")
